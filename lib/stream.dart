6b40a3e2e0796e3ee64903098ce7108543e64b8c
Rfkhdyt100 <rifkiskylad@gmail.com>
2024-12-17 09:15:36 +0700

    soal 2

---
+++ lib/stream.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ColorStream {
+  final List<Color> colors = [
+    Colors.blue,
+    Colors.red,
+    Colors.transparent,
+    Colors.deepOrange,
+    Colors.amber
+  ];
+}
